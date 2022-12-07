ff0af7499eb0207a769c64e4fd65e18c837ad6d5
yeeyemma <119490943+yeeyemma@users.noreply.github.com>
2022-12-07 11:19:49 +0200

    Add files via upload

---
+++ Prezentaciq/src/prezentaciq/Prezentaciq.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prezentaciq;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author momchil.ivanov
+ */
+public class Prezentaciq {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(a+b);
+        // TODO code application logic here
+    }
+    
+}
